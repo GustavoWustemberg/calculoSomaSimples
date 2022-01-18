0baed427db31f56dd15bd137efaa645dbebaf6e4
Gusta <gustavowustemberg14@gmail.com>
2022-01-17 21:45:28 -0300

    Adicionar arquivos de projeto.

---
+++ primeiro/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n1, n2, calc;
+
+Console.WriteLine("Programa de soma");
+Console.WriteLine("digite o primeiro número");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("digite o segundo número");
+n2 = int.Parse(Console.ReadLine());
+calc = n1 + n2;
+Console.WriteLine("O resultado da soma dos dois números é " + calc);
+Console.WriteLine("Aperte qualquer carácter");
+Console.ReadKey();
+return (0);
